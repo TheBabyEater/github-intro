42f42b0e59bc0ef5ad8f37511e7da17849000cd2
Cyrus Cain Allee-Ray <sixbabysixeatersix@gmail.com>
2015-03-04 10:09:32 -0800

    Update Blink.ino

--- arduino/Blink.ino
+++ arduino/Blink.ino
@@ -1,15 +1,15 @@
 
-int led = 13; //13 is wack yo, you might want to change it
+int led = 13; //13 is the pin that you need the LED to be on
 
-void setup() {                
+void setup() {                //for the action that we are going to do
 
-  pinMode(led, OUTPUT);     
+  pinMode(led, OUTPUT);     // states the output
 }
 
 
-void loop() {
-  digitalWrite(led, HIGH);   
-  delay(1000);              
-  digitalWrite(led, LOW);    
-  delay(1000);              
+void loop() {    //lets this action loop
+  digitalWrite(led, HIGH);   //sets the brightness to high
+  delay(1000);              //set amount of waiting time
+  digitalWrite(led, LOW);    //sets the brightness to low
+  delay(1000);              //set amount of waiting time
 }
